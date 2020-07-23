af24cdc46feb85add4add2aa56e92b35c908b1f4
손재이 <jae2son@gmail.com>
2020-07-23 22:21:26 +0900

    723 2D Context

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
 function stopPainting() {
     painting = false;
+}
+
+function startPainting() {
+    painting = true;
 }
 
 function onMouseMove(event) {
@@ -19,10 +27,10 @@ function onMouseDown(event) {
     painting = true;
 }
 
-function onMouseUp(event) {
-    /*painting = false;*/
+/*function onMouseUp(event) {
+    painting = false;
     stopPainting();
-}
+}*/
 
 /*function onMouseLeave(event) {
     painting = false;
@@ -30,8 +38,10 @@ function onMouseUp(event) {
 
 if(canvas) {
     canvas.addEventListener('mousemove', onMouseMove);
-    canvas.addEventListener('mousedown', onMouseDown);
-    canvas.addEventListener('mouseup', onMouseUp);
+    /*canvas.addEventListener('mousedown', onMouseDown);*/
+    canvas.addEventListener('mousedown', startPainting);
+    /*canvas.addEventListener('mouseup', onMouseUp);*/
+    canvas.addEventListener('mouseup', stopPainting);
     /*canvas.addEventListener('mouseleave', onMouseLeave);*/
     canvas.addEventListener('mouseleave', stopPainting);
 }
