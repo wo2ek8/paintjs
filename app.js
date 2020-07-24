d2ff2af0dcb64b1881d69c57e0351589dc440109
손재이 <jae2son@gmail.com>
2020-07-24 21:23:58 +0900

    724 2D Context

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
     /*console.log(x, y);*/
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event) {
