51d5e1b2182a328c9e916c429e258029c518e204
손재이 <jae2son@gmail.com>
2020-07-26 00:13:50 +0900

    Recap!

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
 
+canvas.width = 700;
+canvas.height = 700;
+
 ctx.strokeStyle = '#2c2c2c';
 ctx.lineWidth = 2.5;
 
@@ -19,7 +22,7 @@ function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
     /*console.log(x, y);*/
-    if(!painting) {
+    if (!painting) {
         ctx.beginPath();
         ctx.moveTo(x, y);
     } else {
@@ -29,7 +32,7 @@ function onMouseMove(event) {
 }
 
 function onMouseDown(event) {
-    
+
     /*console.log(event);*/
     painting = true;
 }
@@ -43,7 +46,7 @@ function onMouseDown(event) {
     painting = false;
 }*/
 
-if(canvas) {
+if (canvas) {
     canvas.addEventListener('mousemove', onMouseMove);
     /*canvas.addEventListener('mousedown', onMouseDown);*/
     canvas.addEventListener('mousedown', startPainting);
