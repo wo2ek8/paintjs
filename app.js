c0a341e8a3e8e4b987eaaec38abfad81aa1708e9
손재이 <jae2son@gmail.com>
2020-07-31 18:48:01 +0900

    Filling Mode

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ const colors = document.getElementsByClassName('jsColor');
 const range = document.getElementById('jsRange');
 const mode = document.getElementById('jsMode');
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = '#2c2c2c';
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = '#2c2c2c';
+/*canvas.width = 700;
+canvas.height = 700;*/
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+//ctx.strokeStyle = '#2c2c2c';
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
+/*ctx.fillStyle = 'green';
+ctx.fillRect(50, 20, 100, 40);
+ctx.fillStyle = 'purple';
+ctx.fillRect(80, 100, 100, 40);*/
 
 let painting = false;
 let filling = false;
@@ -55,6 +66,7 @@ function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     //console.log(color);
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
@@ -71,6 +83,15 @@ function handleModeClick(event) {
     } else {
         filling = true;
         mode.innerText = 'Paint';
+        //ctx.fillStyle = ctx.strokeStyle;
+    }
+}
+
+function handleCanvasClick() {
+    //ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    //ctx.fillRect(0, 0, canvas.width, canvas.height);
+    if(filling) {
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
     }
 }
 
@@ -82,6 +103,7 @@ if (canvas) {
     canvas.addEventListener('mouseup', stopPainting);
     /*canvas.addEventListener('mouseleave', onMouseLeave);*/
     canvas.addEventListener('mouseleave', stopPainting);
+    canvas.addEventListener('click', handleCanvasClick);
 }
 
 //console.log(Array.from(colors));
